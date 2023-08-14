702ffcae67a5d9a783995fe7745662f109336446
Augustino Elisaia <99278056+elisaia55@users.noreply.github.com>
2023-08-14 13:09:41 -0700

    Time: 65 ms (92.31%), Space: 44.7 MB (87.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -8,36 +8,27 @@
 
 
 function numIslands(grid) {
-    // counts # of islands
-    let islandCounter = 0;
-    
-    // iterate over each cell in adjacency matrix
-    for ( let row = 0; row < grid.length; row++)
-    for ( let col = 0; col <grid[row].length; col++) {
-        if (grid[row][col] === '1') {
+    islandCounter = 0;
+    for (let row = 0; row < grid.length; row++)
+    for (let col = 0; col < grid[row].length; col++) {
+        if (grid[row][col] === "1") {
             islandCounter++;
-            dfs(row, col, grid);
+            dfs(row, col, grid)
         }
     }
     return islandCounter
 }
-
-// helper "depth first search" func to show visited islands; if visited change cell to "0"
+   
 function dfs(row, col, grid) {
-    
-    // base case to check if out of bounds, 
-    if (row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] === "0") {
-        return 
+    if ( row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] === "0") {
+        return
     }
-    // if in bounds, lets visit each cell 
+    
     grid[row][col] = "0"
     
-    // right of visited 
     dfs(row, col + 1, grid)
-    // left of visited 
-    dfs(row, col - 1, grid)    
-    // top of visited 
+    dfs(row, col - 1, grid)
     dfs(row + 1, col, grid)
-    // bottom of visited 
-    dfs(row -1, col, grid)
+    dfs(row - 1, col, grid)
+   
 }
