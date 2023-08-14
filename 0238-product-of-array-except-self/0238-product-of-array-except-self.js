b8ce4658d82eeb7eaed12917f076cc0567dffa63
Augustino Elisaia <99278056+elisaia55@users.noreply.github.com>
2023-08-14 13:56:47 -0700

    Time: 81 ms (94.18%), Space: 55.8 MB (25.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,18 +2,25 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// 
+
+
 var productExceptSelf = function(nums) {
-    let results = [];
-    let prefix = 1;
-    let postfix = 1;
+let results = [];
+let prefix = 1;
+let postfix = 1;
     
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++){
         results[i] = prefix;
         prefix *= nums[i]
     }
-    for (let i = nums.length - 1; i >= 0; i--){
+    for (let i = nums.length -1; i >= 0; i--){
         results[i] *= postfix;
         postfix *= nums[i]
-    } 
-    return results
+    } return results
+    
+    
+    
+    
 };
