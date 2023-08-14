5040f1234470c49b028f2b840d9aaaaeccb5e122
Augustino Elisaia <99278056+elisaia55@users.noreply.github.com>
2023-08-14 12:13:52 -0700

    Time: 67 ms (89.27%), Space: 44.8 MB (77.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,38 +2,42 @@
  * @param {character[][]} grid
  * @return {number}
  */
-function numIslands(grid) {
-  if (grid === null || grid.length === 0) {
-    return 0;
-  }
-  
-  const m = grid.length;
-  const n = grid[0].length;
-  let count = 0;
 
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (grid[i][j] === '1') {
-        count++;
-        dfs(grid, i, j, m, n);
-      }
+
+
+
+
+function numIslands(grid) {
+    // counts # of islands
+    let islandCounter = 0;
+    
+    // iterate over each cell in adjacency matrix
+    for ( let row = 0; row < grid.length; row++)
+    for ( let col = 0; col <grid[row].length; col++) {
+        if (grid[row][col] === '1') {
+            islandCounter++;
+            dfs(row, col, grid);
+        }
     }
-  }
-  
-  return count;
+    return islandCounter
 }
 
-function dfs(grid, i, j, m, n) {
-  if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === '0') {
-    return;
-  }
-
-  // Mark the current cell as visited
-  grid[i][j] = '0';
-
-  // Visit the neighboring cells in up, down, left, and right directions
-  dfs(grid, i - 1, j, m, n);
-  dfs(grid, i + 1, j, m, n);
-  dfs(grid, i, j - 1, m, n);
-  dfs(grid, i, j + 1, m, n);
+// helper "depth first search" func to show visited islands; if visited change cell to "0"
+function dfs(row, col, grid) {
+    
+    // base case to check if out of bounds, 
+    if (row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] === "0") {
+        return 
+    }
+    // if in bounds, lets visit each cell 
+    grid[row][col] = "0"
+    
+    // right of visited 
+    dfs(row, col + 1, grid)
+    // left of visited 
+    dfs(row, col - 1, grid)    
+    // top of visited 
+    dfs(row + 1, col, grid)
+    // bottom of visited 
+    dfs(row -1, col, grid)
 }
