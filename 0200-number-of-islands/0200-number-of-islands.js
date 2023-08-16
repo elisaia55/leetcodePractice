1164a656a0dd62cade214fdd47aae03d2fd0aaf0
Augustino Elisaia <99278056+elisaia55@users.noreply.github.com>
2023-08-15 20:41:05 -0700

    Time: 65 ms (91.92%), Space: 44.9 MB (77.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -4,31 +4,84 @@
  */
 
 
-
+// iterate over the adjacency matrix from rows/ col 
+// counter for each visited island that are connected 
+// after we check neighbors of visited islands to show for islands that are connected (top, bottom, left , right)
+// we return the counter of islands once fully traversed. (dfs- recursion)
 
 
 function numIslands(grid) {
-    islandCounter = 0;
-    for (let row = 0; row < grid.length; row++)
+   let islandCounter = 0;
+    
+    for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[row].length; col++) {
         if (grid[row][col] === "1") {
             islandCounter++;
-            dfs(row, col, grid)
+            exploreIslands(row, col, grid)
         }
     }
+    }
     return islandCounter
+    
 }
-   
-function dfs(row, col, grid) {
-    if ( row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] === "0") {
+
+function exploreIslands(row, col, grid) {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[row].length || grid[row][col] === "0") {
         return
+    }
+    grid[row][col] = "0"
+    
+    exploreIslands(row + 1, col, grid);
+    exploreIslands(row - 1, col, grid);
+    exploreIslands(row, col + 1, grid);
+    exploreIslands(row, col - 1, grid)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* island counter
+    // iterate over rows, cols
+    // dfs helper func to show visited 
+  let islandCount = 0;
+    for (let row = 0; row < grid.length; row++)
+    for (let col = 0; col < grid[row].length; col++) {
+        if (grid[row][col] === "1") {
+            islandCount++;
+            exploreIslands(row, col, grid)
+        }
+    }
+    return islandCount
+  
+}
+
+function exploreIslands(row, col, grid) {
+    if (row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] === "0") {
+        return 
     }
     
     grid[row][col] = "0"
     
-    dfs(row, col + 1, grid)
-    dfs(row, col - 1, grid)
-    dfs(row + 1, col, grid)
-    dfs(row - 1, col, grid)
-   
-}
+    exploreIslands(row, col + 1, grid)
+    exploreIslands(row, col - 1, grid)
+    exploreIslands(row + 1, col, grid)
+    exploreIslands(row - 1, col, grid)
+   */
