23b3e4933d6465b7d30f3837b249808afd12c980
Augustino Elisaia <99278056+elisaia55@users.noreply.github.com>
2023-10-07 18:40:36 -0700

    Time: 55 ms (35.9%), Space: 42 MB (69.41%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    let start = head , cur = head
+    let i = 1;
+    while (i < left) {
+        start = cur;
+        cur = cur.next;
+        i++;
+    }
+    let prev = null, tail = cur;
+    while (i <= right) {
+        let next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+        i++;
+    }
+    start.next = prev;
+    tail.next = cur;
+    return left == 1 ? prev : head; 
+    // if m == 1, we have no need to connect start list with reversed list reversed list itself is the start (or the head)
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+
+};
