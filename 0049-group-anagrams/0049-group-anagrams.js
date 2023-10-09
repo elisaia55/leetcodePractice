3bf289501756cd7b81c6747318b8acb20e2f209c
Augustino Elisaia <99278056+elisaia55@users.noreply.github.com>
2023-10-08 20:51:39 -0700

    Time: 95 ms (92.08%), Space: 54.6 MB (32.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -6,21 +6,33 @@ var groupAnagrams = function(strs) {
     
     // sort each string, use that as key value pairs in a map, when sorted, if string matches a key we add it to the object 
     
-    const map = {}
-    
-    for (let str of strs) {
-        const newSort = str
-        .split('')
-        .sort((a,b) => a.localeCompare(b))
-        .join(''); 
-        
-    if (!map[newSort]) {
-        map[newSort] = [str];
-    } else {
-        map[newSort].push(str)
+    const anagramMap = new Map();
+
+    for (const str of strs) {
+        sortedStr = str.split('').sort().join('');
+        if (!anagramMap.has(sortedStr)) {
+            anagramMap.set(sortedStr, [])
+        }
+        anagramMap.get(sortedStr).push(str)
     }
-    } 
-    return Object.values(map)
+        return [...anagramMap.values()]
+
+
+    // const map = {}
+    
+    // for (let str of strs) {
+    //     const newSort = str
+    //     .split('')
+    //     .sort((a,b) => a.localeCompare(b))
+    //     .join(''); 
+        
+    // if (!map[newSort]) {
+    //     map[newSort] = [str];
+    // } else {
+    //     map[newSort].push(str)
+    // }
+    // } 
+    // return Object.values(map)
     
     
     
